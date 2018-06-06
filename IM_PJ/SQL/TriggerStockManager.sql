d29113e420cfcd31ed5da6fe7fb1d6d365c7e30f
Binh Tran <tranthaibinh111@yahoo.com>
2018-06-06 18:01:42 +0700

    Fix bug case POS
    - Hien tai: Khi POS nhap qua so luong trong kho thi dang su ly sai
    - Fix bug: Update trigger truong hop QuantityLeft < 0 thi nhap kho chenh lech
    - Fix unicode file sql -> UTF8 standard

--- IM_PJ/SQL/TriggerStockManager.sql
+++ IM_PJ/SQL/TriggerStockManager.sql
@@ -1,4 +1,4 @@
-CREATE Trigger Tr_StockManager
+﻿CREATE Trigger Tr_StockManager
 ON tbl_StockManager
 AFTER INSERT
 AS
@@ -9,13 +9,19 @@ BEGIN
     DECLARE @AgentID AS INT
     DECLARE @ProductID AS INT
     DECLARE @ProductVariableID AS INT
+    DECLARE @Quantity AS FLOAT
     DECLARE @QuantityCurrent AS FLOAT
+    DECLARE @Type AS INT
+    DECLARE @CreatedDate AS DATETIME
 
     SELECT TOP 1
           @ID = ID
         , @AgentID = AgentID
         , @ProductID = ProductID
         , @ProductVariableID = ProductVariableID
+        , @Quantity = Quantity
+        , @Type = Type
+        , @CreatedDate = CreatedDate
     FROM
         tbl_StockManager AS STM
     ORDER BY
@@ -63,6 +69,65 @@ BEGIN
         #StockTarget AS STG
     WHERE
         STG.ROWNUMBER = 2
+
+    IF @Type = 2 -- Xuất kho
+    BEGIN
+        DECLARE @QuantityLeft AS FLOAT
+
+        SET @QuantityLeft = @QuantityCurrent - @Quantity
+
+        IF @QuantityLeft < 0
+        BEGIN
+            INSERT INTO tbl_StockManager (
+                    AgentID
+            ,       ProductID
+            ,       ProductVariableID
+            ,       Quantity
+            ,       QuantityCurrent
+            ,       Type
+            ,       CreatedDate
+            ,       CreatedBy
+            ,       ModifiedDate
+            ,       ModifiedBy
+            ,       NoteID
+            ,       OrderID
+            ,       Status
+            ,       SKU
+            ,       MoveProID
+            ,       ParentID
+            )
+            SELECT
+                    @AgentID
+            ,       @ProductID
+            ,       @ProductVariableID
+            ,       (-1 * @QuantityLeft)
+            ,       @QuantityCurrent
+            ,       1
+            ,       DATEADD(Millisecond, -10, CreatedDate)
+            ,       CreatedBy
+            ,       NULL
+            ,       NULL
+            ,       N'Nhập kho bị lệch khi bán POS'
+            ,       OrderID
+            ,       Status
+            ,       SKU
+            ,       MoveProID
+            ,       ParentID
+            FROM
+                    tbl_StockManager AS STM
+            WHERE
+                    ID = @ID
+            AND     AgentID = @AgentID
+            AND     ProductID = @ProductID
+            AND     ProductVariableID = @ProductVariableID
+            ;
+
+            IF @@ROWCOUNT > 0
+            BEGIN
+                SET @QuantityCurrent = @Quantity
+            END
+        END
+    END
 
     UPDATE tbl_StockManager
     SET QuantityCurrent = @QuantityCurrent
